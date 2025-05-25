ea1dcbd37ecf8048a306d1a562f3fc2244c34dce
AC-Gul <alexgulewich@gmail.com>
2025-05-25 14:20:27 -0700

    Added timer controls

--- Components/Stopwatch.tsx
+++ Components/Stopwatch.tsx
@@ -9,15 +9,31 @@ import { useEffect, useState } from "react";
   let minute = 0;
   let second = 0;
   let ms = 0;
+  let isPaused = true;
+
 
 // "HH:MM:SS" format
 export default function Clock() {
 
     const [currentTime, SetTime] = useState("00:00:00:00");
 
+      function ResetTimer() 
+  {
+    hour = 0;
+    minute = 0;
+    second = 0;
+    ms = 0;
+    SetTime("00:00:00:00");
+    isPaused = true;
+  }
+
     // Update the timer every second
     useEffect(() => {
     const interval = setInterval(() => {
+      if (isPaused) {
+        return;
+      }
+
         ms += 1;
         if (ms >= 100) {
             ms = 0;
@@ -41,12 +57,18 @@ export default function Clock() {
     return () => clearInterval(interval);
     })
 
+    
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen">
       <h1 className="text-text text-4xl font-bold mb-4">Clock</h1>
       <div className="text-text text-2xl">
         {currentTime}
       </div>
+
+      <button className="bg-primary text-text" onClick={() => isPaused=false}>Start</button>
+      <button className="bg-primary text-text" onClick={() => ResetTimer()}>Reset</button>
+      <button className="bg-primary text-text" onClick={() => isPaused=true}>Stop</button>
+
     </div>
   );
 }
